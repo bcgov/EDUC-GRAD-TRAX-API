e5497c670b813964596fa6fffa9d66cb8f0ee15e
MCHINTHA <mamatha.chinthakindi@gov.bc.ca>
2023-03-10 15:41:29 -0800

    GRAD2-1929 Refactoring/Linting Grad Trax API

--- api/src/test/java/ca/bc/gov/educ/api/trax/controller/SchoolControllerTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/trax/controller/SchoolControllerTest.java
@@ -28,7 +28,7 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
-@SuppressWarnings("rawtypes")
+@SuppressWarnings("ALL")
 public class SchoolControllerTest {
 
     @Mock
