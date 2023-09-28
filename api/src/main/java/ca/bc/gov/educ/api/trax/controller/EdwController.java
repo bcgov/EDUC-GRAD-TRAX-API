3c10285a8cc6f9f28a7d3b789b58daa2c8d6f798
Jinil Sung <infstar@gmail.com>
2023-09-28 12:00:57 -0700

    Removed the code smells.
    Removed the code smells.

--- api/src/main/java/ca/bc/gov/educ/api/trax/controller/EdwController.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/controller/EdwController.java
@@ -1,10 +1,7 @@
 package ca.bc.gov.educ.api.trax.controller;
 
 import ca.bc.gov.educ.api.trax.model.dto.SnapshotResponse;
-import ca.bc.gov.educ.api.trax.model.dto.TranscriptStudentCourse;
-import ca.bc.gov.educ.api.trax.model.dto.TranscriptStudentDemog;
 import ca.bc.gov.educ.api.trax.service.EdwService;
-import ca.bc.gov.educ.api.trax.service.TswService;
 import ca.bc.gov.educ.api.trax.util.EducGradTraxApiConstants;
 import ca.bc.gov.educ.api.trax.util.GradValidation;
 import ca.bc.gov.educ.api.trax.util.PermissionsConstants;
@@ -34,6 +31,9 @@ public class EdwController {
 
     private static Logger logger = LoggerFactory.getLogger(EdwController.class);
 
+    private static final String GRAD_YEAR_PARAM = "GradYear";
+    private static final String SCHOOL_PARAM = "MinCode";
+
     @Autowired
     EdwService edwService;
     
@@ -50,7 +50,7 @@ public class EdwController {
             @ApiResponse(responseCode = "400", description = "BAD REQUEST")})
     public ResponseEntity<List<String>> getSchoolListFromSnapshotByGradYear(@PathVariable Integer gradYear) {
         logger.debug("getSchoolListFromSnapshotByGradYear : ");
-        validation.requiredField(gradYear, "GradYear");
+        validation.requiredField(gradYear, GRAD_YEAR_PARAM);
         if (validation.hasErrors()) {
             validation.stopOnErrors();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -65,7 +65,7 @@ public class EdwController {
             @ApiResponse(responseCode = "400", description = "BAD REQUEST")})
     public ResponseEntity<List<SnapshotResponse>> getStudentsFromSnapshotByGradYear(@PathVariable Integer gradYear) {
         logger.debug("getStudentsFromSnapshotByGradYear : ");
-        validation.requiredField(gradYear, "GradYear");
+        validation.requiredField(gradYear, GRAD_YEAR_PARAM);
         if (validation.hasErrors()) {
             validation.stopOnErrors();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -80,8 +80,8 @@ public class EdwController {
             @ApiResponse(responseCode = "400", description = "BAD REQUEST")})
     public ResponseEntity<List<SnapshotResponse>> getStudentsFromSnapshotByGradYearAndSchool(@PathVariable Integer gradYear, @PathVariable String minCode) {
         logger.debug("getStudentsFromSnapshotByGradYearAndSchool : ");
-        validation.requiredField(gradYear, "GradYear");
-        validation.requiredField(minCode, "MinCode");
+        validation.requiredField(gradYear, GRAD_YEAR_PARAM);
+        validation.requiredField(minCode, SCHOOL_PARAM);
         if (validation.hasErrors()) {
             validation.stopOnErrors();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
