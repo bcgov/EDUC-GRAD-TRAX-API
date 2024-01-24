686d89efbab005d89e58244f8b742b13b3b0352c
Kamal Mohammed <kamal.mohammed@outlook.com>
2024-01-24 13:53:11 -0700

    Update Actions

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -44,7 +44,7 @@ echo Creating config map "$APP_NAME"-config-map
 oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-config-map \
   --from-literal=APP_LOG_LEVEL="$APP_LOG_LEVEL" \
   --from-literal=BASELINE_ON_MIGRATE="true" \
-  --from-literal=BASELINE_ON_MIGRATE="0 0/5 * * * *" \
+  --from-literal=CRON_SCHEDULED_GRAD_TO_TRAX_EVENTS="0 0/5 * * * *" \
   --from-literal=CRON_SCHEDULED_GRAD_TO_TRAX_EVENTS_LOCK_AT_LEAST_FOR="PT1M" \
   --from-literal=CRON_SCHEDULED_GRAD_TO_TRAX_EVENTS_LOCK_AT_MOST_FOR="PT5M" \
   --from-literal=CRON_SCHEDULED_GRAD_TO_TRAX_EVENTS_THRESHOLD="1000" \
