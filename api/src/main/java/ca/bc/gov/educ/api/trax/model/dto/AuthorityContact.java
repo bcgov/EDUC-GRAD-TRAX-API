8f098f5840e483e3e7598c941ad863bd56a21592
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2024-05-29 09:03:13 -0700

    Patch for adding default no args constructors to model.

--- api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/AuthorityContact.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/AuthorityContact.java
@@ -1,13 +1,21 @@
 package ca.bc.gov.educ.api.trax.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.io.Serializable;
+
 @Data
-@Builder
 @EqualsAndHashCode(callSuper = true)
-public class AuthorityContact extends BaseModel {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AuthorityContact extends BaseModel implements Serializable {
+
+    /**
+     * The constant serialVersionUID.
+     */
+    private static final long serialVersionUID = 1L;
 
     private String authorityContactId;
 
--- api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/SchoolContact.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/SchoolContact.java
@@ -1,13 +1,22 @@
 package ca.bc.gov.educ.api.trax.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
-@Builder
 @EqualsAndHashCode(callSuper = true)
-public class SchoolContact extends BaseModel {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SchoolContact extends BaseModel implements Serializable {
+
+    /**
+     * The constant serialVersionUID.
+     */
+    private static final long serialVersionUID = 1L;
 
     private String schoolContactId;
 
@@ -34,5 +43,6 @@ public class SchoolContact extends BaseModel {
     private String effectiveDate;
 
     private String expiryDate;
+
 
 }
