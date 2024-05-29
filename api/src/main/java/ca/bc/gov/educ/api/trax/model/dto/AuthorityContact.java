681315a5a9ff2e3c57fd658560984f027e4a1fdc
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2024-05-29 09:26:47 -0700

    Code cleanup

--- api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/AuthorityContact.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/AuthorityContact.java
@@ -1,7 +1,6 @@
 package ca.bc.gov.educ.api.trax.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
--- api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/SchoolContact.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/SchoolContact.java
@@ -1,10 +1,8 @@
 package ca.bc.gov.educ.api.trax.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
