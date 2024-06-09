233a51bca12eba4c6a32d388f8d868a3c98da997
Kamal Mohammed <kamal.mohammed@outlook.com>
2024-06-09 04:33:09 -0600

    GRAD2-2637 - Cache institute api data into Redis

--- api/src/main/java/ca/bc/gov/educ/api/trax/cache/CacheInitializer.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/cache/CacheInitializer.java
@@ -6,8 +6,10 @@ import ca.bc.gov.educ.api.trax.service.institute.SchoolService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("!test")
 @Slf4j
 @Component
 public class CacheInitializer implements CommandLineRunner {
