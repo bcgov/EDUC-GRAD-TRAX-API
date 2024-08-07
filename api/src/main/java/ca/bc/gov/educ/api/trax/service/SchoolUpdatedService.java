890d835ee89a5cbeaaeab79f229dd988e7ae2bed
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2024-08-07 08:53:53 -0700

    Fixing duplication of code.

--- api/src/main/java/ca/bc/gov/educ/api/trax/service/SchoolUpdatedService.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/service/SchoolUpdatedService.java
@@ -1,9 +1,6 @@
 package ca.bc.gov.educ.api.trax.service;
 
 import ca.bc.gov.educ.api.trax.constant.EventType;
-import ca.bc.gov.educ.api.trax.exception.ServiceException;
-import ca.bc.gov.educ.api.trax.model.dto.institute.School;
-import ca.bc.gov.educ.api.trax.model.entity.EventEntity;
 import ca.bc.gov.educ.api.trax.service.institute.SchoolService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,24 +8,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class SchoolUpdatedService extends EventBaseService<School> {
-
-    SchoolService schoolService;
+public class SchoolUpdatedService extends SchoolCreatedService {
 
     @Autowired
     public SchoolUpdatedService(SchoolService schoolService) {
-        this.schoolService = schoolService;
-    }
-
-    @Override
-    public void processEvent(final School school, EventEntity eventEntity) {
-        log.debug("Processing School Updated");
-        try{
-            schoolService.updateSchoolCache(school.getSchoolId());
-            this.updateEventWithHistory(eventEntity);
-        } catch (ServiceException e) {
-            log.error(e.getMessage());
-        }
+        super(schoolService);
     }
 
     @Override
