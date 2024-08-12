426e12f391e6f4ed53c6018a77d984b76a16b3e3
Chris Ditcher <chris.ditcher@gov.bc.ca>
2024-08-12 15:44:39 -0700

    GRAD2-2763 Add new activity codes for Institute (#344)
    * Added activity code checking.
    
    * Updated test coverage.
    
    * Increased test coverage.
    
    ---------
    
    Co-authored-by: chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>

--- api/src/test/java/ca/bc/gov/educ/api/trax/service/EventHandlerDelegatorServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/trax/service/EventHandlerDelegatorServiceTest.java
@@ -1,30 +1,108 @@
 package ca.bc.gov.educ.api.trax.service;
 
+import ca.bc.gov.educ.api.trax.choreographer.ChoreographEventHandler;
+import ca.bc.gov.educ.api.trax.constant.EventActivityCode;
+import ca.bc.gov.educ.api.trax.messaging.NatsConnection;
+import ca.bc.gov.educ.api.trax.messaging.jetstream.Publisher;
+import ca.bc.gov.educ.api.trax.messaging.jetstream.Subscriber;
 import ca.bc.gov.educ.api.trax.model.dto.ChoreographedEvent;
+import ca.bc.gov.educ.api.trax.model.entity.EventEntity;
 import io.nats.client.Message;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import redis.clients.jedis.JedisCluster;
+
 
 import java.io.IOException;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static ca.bc.gov.educ.api.trax.constant.Topics.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
-public class EventHandlerDelegatorServiceTest extends BaseReplicationServiceTest {
+@SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
+class EventHandlerDelegatorServiceTest {
 
-    @MockBean
+    @SpyBean
     private EventHandlerDelegatorService eventHandlerDelegatorService;
-
     @MockBean
-    private ChoreographedEvent choreographedEvent;
-
+    private ChoreographedEventPersistenceService choreographedEventPersistenceService;
+    @MockBean
+    private ChoreographEventHandler choreographer;
     @MockBean
     private Message message;
+    @MockBean
+    private JedisConnectionFactory jedisConnectionFactoryMock;
+    @MockBean
+    private JedisCluster jedisClusterMock;
+    @MockBean
+    public Publisher publisher;
+    @MockBean
+    public Subscriber subscriber;
+    @MockBean
+    public NatsConnection natsConnection;
+    @MockBean
+    public ClientRegistrationRepository clientRegistrationRepository;
+    @MockBean
+    public OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+    @MockBean
+    public OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+    private EventEntity eventEntity;
+    private ChoreographedEvent choreographedEvent;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        choreographedEvent = new ChoreographedEvent();
+        eventEntity = new EventEntity();
+        eventEntity.setEventType("UPDATE_SCHOOL");
+        when(message.getSubject()).thenReturn(INSTITUTE_EVENTS_TOPIC.toString());
+        when(choreographedEventPersistenceService.persistEventToDB(any())).thenReturn(eventEntity);
+    }
 
     @Test
-    public void whenHandleChoreographyEvent_thenVerified() throws IOException {
+    void whenHandleChoreographyEvent_thenVerified() throws IOException {
         this.eventHandlerDelegatorService.handleChoreographyEvent(this.choreographedEvent, this.message);
         verify(eventHandlerDelegatorService, times(1)).handleChoreographyEvent(this.choreographedEvent, this.message);
     }
+
+    @Test
+    void testSetActivityCode_givenINSTITUTE_EVENT_shouldSetINSTITUTE_EVENTActivityCode() throws IOException {
+        this.eventHandlerDelegatorService.handleChoreographyEvent(this.choreographedEvent, this.message);
+        assertTrue(choreographedEvent.getActivityCode().equalsIgnoreCase(EventActivityCode.INSTITUTE_EVENT.toString()));
+    }
+
+    @Test
+    void testSetActivityCode_givenPEN_EVENT_shouldSetPEN_EVENTActivityCode() throws IOException {
+        when(message.getSubject()).thenReturn(PEN_EVENTS_TOPIC.toString());
+        this.eventHandlerDelegatorService.handleChoreographyEvent(this.choreographedEvent, this.message);
+        assertTrue(choreographedEvent.getActivityCode().equalsIgnoreCase(EventActivityCode.PEN_EVENT.toString()));
+    }
+
+    @Test
+    void testSetActivityCode_givenCOREG_EVENT_shouldSetCOREG_EVENTActivityCode() throws IOException {
+        when(message.getSubject()).thenReturn(COREG_EVENTS_TOPIC.toString());
+        this.eventHandlerDelegatorService.handleChoreographyEvent(this.choreographedEvent, this.message);
+        assertTrue(choreographedEvent.getActivityCode().equalsIgnoreCase(EventActivityCode.COREG_EVENT.toString()));
+    }
+
+    @Test
+    void testSetActivityCode_givenInvalidCode_shouldLogError(CapturedOutput capturedOutput) throws IOException {
+        final String topic = "SILLY_EVENTS_TOPIC";
+        final String errorMessage = String.format("%s is not a valid topic", topic);
+        when(message.getSubject()).thenReturn(topic);
+        this.eventHandlerDelegatorService.handleChoreographyEvent(this.choreographedEvent, this.message);
+        assertTrue(capturedOutput.getAll().contains(errorMessage));
+    }
+
 
 }
