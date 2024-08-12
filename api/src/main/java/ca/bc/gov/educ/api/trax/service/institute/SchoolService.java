3a3c81132974683fb38a353316615efd0d27b707
Kamal Mohammed <kamal.mohammed@outlook.com>
2024-08-12 03:21:35 -0600

    GRAD2-2638 - Institute - Create V2 endpoints for School data

--- api/src/main/java/ca/bc/gov/educ/api/trax/service/institute/SchoolService.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/service/institute/SchoolService.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
