1b1ca0d5a78c4054466ef95367cdf6c2746ec33d
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2024-08-14 09:46:40 -0700

    Reducing code duplication

--- api/src/main/java/ca/bc/gov/educ/api/trax/service/DistrictContactDeletedService.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/service/DistrictContactDeletedService.java
@@ -1,9 +1,6 @@
 package ca.bc.gov.educ.api.trax.service;
 
 import ca.bc.gov.educ.api.trax.constant.EventType;
-import ca.bc.gov.educ.api.trax.exception.ServiceException;
-import ca.bc.gov.educ.api.trax.model.dto.DistrictContact;
-import ca.bc.gov.educ.api.trax.model.entity.EventEntity;
 import ca.bc.gov.educ.api.trax.service.institute.DistrictService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,25 +8,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class DistrictContactDeletedService extends EventBaseService<DistrictContact> {
-
-    DistrictService districtService;
+public class DistrictContactDeletedService extends DistrictContactEventBaseService {
 
     @Autowired
     public DistrictContactDeletedService(DistrictService districtService) {
-        this.districtService = districtService;
-    }
-
-    @Override
-    public void processEvent(final DistrictContact districtContact, EventEntity eventEntity) {
-        log.debug("Processing District Contact Deleted");
-        try {
-            districtService.updateDistrictCache(districtContact.getDistrictId());
-            this.updateEvent(eventEntity);
-        } catch (ServiceException e) {
-            // do not mark eventEntity as processed
-            log.error(e.getMessage());
-        }
+        super(districtService);
     }
 
     @Override
--- api/src/main/java/ca/bc/gov/educ/api/trax/service/SchoolContactCreatedService.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/service/SchoolContactCreatedService.java
@@ -1,9 +1,6 @@
 package ca.bc.gov.educ.api.trax.service;
 
 import ca.bc.gov.educ.api.trax.constant.EventType;
-import ca.bc.gov.educ.api.trax.exception.ServiceException;
-import ca.bc.gov.educ.api.trax.model.dto.SchoolContact;
-import ca.bc.gov.educ.api.trax.model.entity.EventEntity;
 import ca.bc.gov.educ.api.trax.service.institute.SchoolService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,26 +8,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class SchoolContactCreatedService extends EventBaseService<SchoolContact> {
-
-    SchoolService schoolService;
+public class SchoolContactCreatedService extends SchoolContactEventBaseService {
 
     @Autowired
     public SchoolContactCreatedService(SchoolService schoolService) {
-        this.schoolService = schoolService;
-    }
-
-    @Override
-    public void processEvent(final SchoolContact schoolContact, EventEntity eventEntity) {
-        log.debug("Processing School Contact Created");
-        try {
-            schoolService.updateSchoolCache(schoolContact.getSchoolId());
-            this.updateEvent(eventEntity);
-        } catch (ServiceException e) {
-            // do not mark eventEntity as processed
-            log.error(e.getMessage());
-        }
-
+        super(schoolService);
     }
 
     @Override
