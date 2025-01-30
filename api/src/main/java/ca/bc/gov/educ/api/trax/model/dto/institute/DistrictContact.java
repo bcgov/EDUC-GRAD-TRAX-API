87155d29c93655e528cae00be273d567263f178e
Kamal Mohammed <kamal.mohammed@outlook.com>
2025-01-29 17:00:39 -0700

    GRAD2-3256 - Remove the check on displayNamenoCharacters (#412)

--- api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/institute/DistrictContact.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/model/dto/institute/DistrictContact.java
@@ -1,30 +1,26 @@
 package ca.bc.gov.educ.api.trax.model.dto.institute;
 
 import ca.bc.gov.educ.api.trax.model.dto.BaseModel;
-import ca.bc.gov.educ.api.trax.model.entity.institute.DistrictAddressEntity;
-import ca.bc.gov.educ.api.trax.model.entity.institute.DistrictContactEntity;
-import ca.bc.gov.educ.api.trax.model.entity.institute.NoteEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Component("districtContact")
 public class DistrictContact extends BaseModel {
 
+    private String districtContactId;
     private String districtId;
-    private String districtNumber;
-    private String faxNumber;
+    private String districtContactTypeCode;
     private String phoneNumber;
+    private String jobTitle;
+    private String phoneExtension;
+    private String alternatePhoneNumber;
+    private String alternatePhoneExtension;
     private String email;
-    private String website;
-    private String districtRegionCode;
-    private String districtStatusCode;
-    private List<DistrictContactEntity> contacts;
-    private List<DistrictAddressEntity> addresses;
-    private List<NoteEntity> notes;
-
+    private String firstName;
+    private String lastName;
+    private String effectiveDate;
+    private String expiryDate;
 }
