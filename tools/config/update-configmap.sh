3804ad20067a1d6b5dc47c1b784835ca729daa75
Kamal Mohammed <kamal.mohammed@outlook.com>
2025-02-14 01:46:51 +0530

    Update update-configmap.sh

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -94,7 +94,7 @@ oc create -n "$GRAD_NAMESPACE"-"$envValue" configmap "$APP_NAME"-flb-sc-config-m
 REDIS_SECRET=$(oc get secret -n "$GRAD_NAMESPACE"-"$envValue" redis-ha -o json | jq --raw-output '.data.REDIS_PASSWORD' | base64 --decode)
 oc create secret generic -n "$GRAD_NAMESPACE"-"$envValue" redis-cache-secret \
   --from-literal=REDIS_URL=$9 \
-  --from-literal=REDIS_PORT=$10 \
-  --from-literal=REDIS_USER=$11 \
+  --from-literal=REDIS_PORT="$10" \
+  --from-literal=REDIS_USER="$11" \
   --from-literal=REDIS_SECRET="$REDIS_SECRET" \
   --dry-run=client -o yaml | oc apply -f -
