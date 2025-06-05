fc443cf932265557a5dd401a6b468f0f90600a94
Kamal Mohammed <kamal.mohammed@outlook.com>
2025-06-05 12:01:35 -0600

    GRAD2-3283 - Fixing 400 Authentication bug

--- api/src/main/java/ca/bc/gov/educ/api/trax/config/GradTraxConfig.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/config/GradTraxConfig.java
@@ -8,12 +8,19 @@ import ca.bc.gov.educ.api.trax.model.entity.PsiEntity;
 import ca.bc.gov.educ.api.trax.model.entity.SchoolEntity;
 import ca.bc.gov.educ.api.trax.util.EducGradTraxApiConstants;
 import ca.bc.gov.educ.api.trax.util.LogHelper;
+import net.javacrumbs.shedlock.core.LockProvider;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @EnableAsync
 @Configuration
@@ -42,6 +49,33 @@ public class GradTraxConfig {
 		modelMapper.typeMap(PsiEntity.class, Psi.class);
 		modelMapper.typeMap(Psi.class, PsiEntity.class);
 		return modelMapper;
+	}
+
+	/**
+	 * Lock provider lock provider.
+	 *
+	 * @param jdbcTemplate       the jdbc template
+	 * @param transactionManager the transaction manager
+	 * @return the lock provider
+	 */
+	@Bean
+	public LockProvider lockProvider(@Autowired JdbcTemplate jdbcTemplate, @Autowired PlatformTransactionManager transactionManager) {
+		return new JdbcTemplateLockProvider(jdbcTemplate, transactionManager, "REPLICATION_SHEDLOCK");
+	}
+
+	/**
+	 * Thread pool task scheduler thread pool task scheduler.
+	 *
+	 * @return the thread pool task scheduler
+	 */
+	@Bean(name = "taskExecutor")
+	public TaskExecutor threadPoolTaskScheduler() {
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		threadPoolTaskExecutor.setThreadNamePrefix("async-");
+		threadPoolTaskExecutor.setCorePoolSize(2);
+		threadPoolTaskExecutor.setMaxPoolSize(5);
+		threadPoolTaskExecutor.initialize();
+		return new DelegatingSecurityContextAsyncTaskExecutor(threadPoolTaskExecutor);
 	}
 
 }
