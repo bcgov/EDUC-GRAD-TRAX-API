83fecde1d53dc3f9512e5762f43b4e49c077e2cb
cditcher <chris.ditcher@gov.bc.ca>
2025-08-13 13:59:52 -0700

    Updated unit tests

---
+++ api/src/test/java/ca/bc/gov/educ/api/trax/service/GradSchoolUpdatedServiceTest.java
@@ -0,0 +1,53 @@
+package ca.bc.gov.educ.api.trax.service;
+
+import ca.bc.gov.educ.api.trax.constant.EventType;
+import ca.bc.gov.educ.api.trax.exception.ServiceException;
+import ca.bc.gov.educ.api.trax.service.institute.SchoolService;
+import ca.bc.gov.educ.api.trax.support.TestUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+
+public class GradSchoolUpdatedServiceTest extends BaseReplicationServiceTest {
+
+    @Autowired
+    private GradSchoolUpdatedService gradSchoolUpdatedService;
+
+    @MockBean
+    private SchoolService schoolServiceMock;
+
+    @Test
+    public void testProcessEvent_givenUPDATE_GRAD_SCHOOL_Event_shouldProcessEvent() throws JsonProcessingException {
+        final var request = TestUtils.createGradSchool();
+        final var event = TestUtils.createEvent(EventType.UPDATE_GRAD_SCHOOL.toString(), request, this.replicationTestUtils.getEventRepository());
+        this.gradSchoolUpdatedService.processEvent(request, event);
+        var result = this.replicationTestUtils.getEventRepository().findById(event.getReplicationEventId());
+        if(result.isPresent()){
+            Assert.assertEquals("PROCESSED", result.get().getEventStatus());
+        } else {
+            fail("UPDATE_GRAD_SCHOOL failed to process");
+        }
+    }
+
+    @Test
+    public void testProcessEvent_givenUPDATE_GRAD_SCHOOL_Event_ServiceUnavailable_triggerError() throws JsonProcessingException {
+        final String ERROR_MSG = "Test Exception";
+        doThrow(new ServiceException(ERROR_MSG)).when(schoolServiceMock).updateSchoolCache(anyString());
+        final var request = TestUtils.createGradSchool();
+        final var event = TestUtils.createEvent(EventType.UPDATE_GRAD_SCHOOL.toString(), request, this.replicationTestUtils.getEventRepository());
+        this.gradSchoolUpdatedService.processEvent(request, event);
+        var result = this.replicationTestUtils.getEventRepository().findById(event.getReplicationEventId());
+        if(result.isPresent()){
+            Assert.assertEquals("DB_COMMITTED", result.get().getEventStatus());
+        } else {
+            fail("UPDATE_GRAD_SCHOOL failed to process");
+        }
+    }
+
+}
