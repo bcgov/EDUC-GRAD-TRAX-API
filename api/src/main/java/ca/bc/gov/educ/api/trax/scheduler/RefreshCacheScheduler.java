0cd1cf9981fd17d5d17409f41df9d200aec05860
cditcher <chris.ditcher@gov.bc.ca>
2025-09-04 09:01:55 -0700

    Added a cache refresh scheduler

---
+++ api/src/main/java/ca/bc/gov/educ/api/trax/scheduler/RefreshCacheScheduler.java
@@ -0,0 +1,50 @@
+package ca.bc.gov.educ.api.trax.scheduler;
+
+import ca.bc.gov.educ.api.trax.service.institute.CodeService;
+import ca.bc.gov.educ.api.trax.service.institute.DistrictService;
+import ca.bc.gov.educ.api.trax.service.institute.SchoolService;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class RefreshCacheScheduler {
+
+    SchoolService schoolService;
+    DistrictService districtService;
+    CodeService codeService;
+
+    @Autowired
+    public RefreshCacheScheduler(SchoolService schoolService, DistrictService districtService, CodeService codeService) {
+        this.schoolService = schoolService;
+        this.districtService = districtService;
+        this.codeService = codeService;
+    }
+
+    @Scheduled(cron = "${cron.scheduled.process.update-cache.run}")
+    @SchedulerLock(name = "RefreshCacheLock",
+            lockAtLeastFor = "${cron.scheduled.process.update-cache.lockAtLeastFor}", lockAtMostFor = "${cron.scheduled.process.update-cache.lockAtMostFor}")
+    @Transactional
+    public void refreshCache() {
+        try {
+            log.debug("Refreshing cache on scheduled process.");
+            LockAssert.assertLocked();
+            log.debug("Refreshing school cache");
+            schoolService.initializeSchoolCache(true);
+            log.debug("Refreshing district cache");
+            districtService.initializeDistrictCache(true);
+            log.debug("Refreshing code cache");
+            codeService.initializeSchoolCategoryCodeCache(true);
+            codeService.initializeSchoolFundingGroupCodeCache(true);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+    }
+
+}
