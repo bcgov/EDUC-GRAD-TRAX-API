e33629a5cbca3054064010bf5cd969dc561ae425
arcshiftsolutions <arcshiftsolutions@gmail.com>
2025-10-02 19:58:12 -0700

    Consolidated scheduler

--- api/src/main/java/ca/bc/gov/educ/api/trax/scheduler/TraxUpdateTriggeredRecordScheduler.java
+++ api/src/main/java/ca/bc/gov/educ/api/trax/scheduler/TraxUpdateTriggeredRecordScheduler.java
@@ -1,7 +1,11 @@
 package ca.bc.gov.educ.api.trax.scheduler;
 
+import ca.bc.gov.educ.api.trax.choreographer.ChoreographEventHandler;
 import ca.bc.gov.educ.api.trax.exception.TraxAPIRuntimeException;
+import ca.bc.gov.educ.api.trax.model.entity.EventEntity;
 import ca.bc.gov.educ.api.trax.model.entity.TraxUpdateInGradEntity;
+import ca.bc.gov.educ.api.trax.model.entity.TraxUpdatedPubEvent;
+import ca.bc.gov.educ.api.trax.repository.EventRepository;
 import ca.bc.gov.educ.api.trax.repository.TraxUpdateInGradRepository;
 import ca.bc.gov.educ.api.trax.repository.TraxUpdatedPubEventRepository;
 import ca.bc.gov.educ.api.trax.service.TraxUpdateService;
@@ -13,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static ca.bc.gov.educ.api.trax.constant.EventStatus.DB_COMMITTED;
@@ -22,14 +27,17 @@ import static ca.bc.gov.educ.api.trax.constant.EventStatus.DB_COMMITTED;
 public class TraxUpdateTriggeredRecordScheduler {
     private final TraxUpdateService traxUpdateService;
     private final  TraxUpdatedPubEventRepository traxUpdatedPubEventRepository;
-
+    private final EventRepository eventRepository;
     private final EducGradTraxApiConstants constants;
+    private final ChoreographEventHandler choreographer;
 
-    public TraxUpdateTriggeredRecordScheduler(final TraxUpdateService traxUpdateService, TraxUpdatedPubEventRepository traxUpdatedPubEventRepository,
-                                              final EducGradTraxApiConstants constants) {
+    public TraxUpdateTriggeredRecordScheduler(final TraxUpdateService traxUpdateService, TraxUpdatedPubEventRepository traxUpdatedPubEventRepository, EventRepository eventRepository,
+                                              final EducGradTraxApiConstants constants, ChoreographEventHandler choreographer) {
         this.traxUpdateService = traxUpdateService;
         this.traxUpdatedPubEventRepository = traxUpdatedPubEventRepository;
+        this.eventRepository = eventRepository;
         this.constants = constants;
+        this.choreographer = choreographer;
     }
 
     @Scheduled(cron = "${cron.scheduled.process.trigger-jobs.read-trax-update.run}") // every 5 minute
@@ -42,23 +50,47 @@ public class TraxUpdateTriggeredRecordScheduler {
             log.info("Event count is greater than 100, so not processing any TRAX records");
             return;
         }
+
+        final var results = this.eventRepository.fetchByEventStatus(List.of(DB_COMMITTED.toString()), constants.getGradToTraxProcessingThreshold());
+        log.info("Number of records found to process from GRAD to TRAX: {}", results.size());
+        if (!results.isEmpty()) {
+            var filteredList = results.stream().filter(el -> el.getUpdateDate().isBefore(LocalDateTime.now().minusMinutes(5))).toList();
+            for (EventEntity e : filteredList) {
+                try {
+                    choreographer.handleEvent(e);
+                } catch (final Exception ex) {
+                    log.error("Exception while trying to handle GRAD updated message", ex);
+                    throw new TraxAPIRuntimeException("Exception while trying to handle GRAD updated message: " + ex.getMessage());
+                }
+            }
+            log.debug("PROCESS_CHOREOGRAPHED_EVENTS_FROM_JET_STREAM: processing is completed");
+        }
+
+        log.info("Running query for TRAX to GRAD updates");
+        final var resultsPubEvent = this.traxUpdatedPubEventRepository.fetchByEventStatus(List.of(DB_COMMITTED.toString()), constants.getTraxToGradProcessingThreshold());
+        log.info("Number of records found to process from TRAX to GRAD: {}", results.size());
+        if (!resultsPubEvent.isEmpty()) {
+            var filteredList = resultsPubEvent.stream().filter(el -> el.getUpdateDate().isBefore(LocalDateTime.now().minusMinutes(5))).toList();
+            filteredList.forEach(traxUpdateService::publishToJetStream);
+            log.debug("PUBLISH_TRAX_UPDATED_EVENTS_TO_JET_STREAM: processing is completed");
+        }
         
         log.debug("PROCESS_TRAX_UPDATE_IN_GRAD_RECORDS: started - cron {}, lockAtMostFor {}", constants.getTraxTriggersCronRun(), constants.getTraxTriggersLockAtMostFor());
-        final var results = traxUpdateService.getOutstandingList(constants.getTraxTriggersProcessingThreshold());
+        final var resultsOutstanding = traxUpdateService.getOutstandingList(constants.getTraxTriggersProcessingThreshold());
         log.info("Number of records found to process from TRAX: {}", results.size());
-        if (!results.isEmpty()) {
+        if (!resultsOutstanding.isEmpty()) {
             try {
-                log.info("Saving {} events for TRAX to GRAD updates", results.size());
-                var events = traxUpdateService.writeTraxUpdatedEvent(results);
-                log.info("Saved {} events for TRAX to GRAD", results.size());
-                log.info("Updating {} event statuses for TRAX to GRAD", results.size());
-                traxUpdateService.updateStatuses(results);
-                log.info("Updated {} event statuses for TRAX to GRAD", results.size());
+                log.info("Saving {} events for TRAX to GRAD updates", resultsOutstanding.size());
+                var events = traxUpdateService.writeTraxUpdatedEvent(resultsOutstanding);
+                log.info("Saved {} events for TRAX to GRAD", resultsOutstanding.size());
+                log.info("Updating {} event statuses for TRAX to GRAD", resultsOutstanding.size());
+                traxUpdateService.updateStatuses(resultsOutstanding);
+                log.info("Updated {} event statuses for TRAX to GRAD", resultsOutstanding.size());
                 
                 if(!events.isEmpty()) {
-                    log.info("Publishing {} events for TRAX to GRAD to jet stream", results.size());
+                    log.info("Publishing {} events for TRAX to GRAD to jet stream", resultsOutstanding.size());
                     events.forEach(traxUpdateService::publishToJetStream);
-                    log.info("Published {} events for TRAX to GRAD to jet stream", results.size());
+                    log.info("Published {} events for TRAX to GRAD to jet stream", resultsOutstanding.size());
                 }
             } catch (final Exception ex) {
                 log.error("Exception while trying to handle update_in_grad records", ex);
